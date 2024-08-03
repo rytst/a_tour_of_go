87d691b44bfb9120789f6f1cb80e2791e82f240e
rytst <r.saito0111@gmail.com>
2024-08-03 15:11:39 +0900

    modify exercise slices

--- basics/exercise_slices.go
+++ basics/exercise_slices.go
@@ -6,15 +6,19 @@ import "golang.org/x/tour/pic"
 
 
 func Pic(dx, dy int) [][]uint8 {
-    array := make([][]uint8, dy)
+    slice := make([][]uint8, dy)
+    for i := range slice {
+        slice[i] = make([]uint8, dx)
+    }
+
     for y := 0; y < dy; y++ {
         for x := 0; x < dx; x++ {
-            z := uint8(x^y)
-            array[y] = append(array[y], z)
+            z := (x + y) / 2
+            slice[y][x] = uint8(z)
         }
     }
 
-    return array
+    return slice
 
 }
 
