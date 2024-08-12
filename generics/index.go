74a81a169ccf00ef4b3c745c82e298c9663a47c3
rytst <r.saito0111@gmail.com>
2024-08-12 20:40:32 +0900

    add comment

--- generics/index.go
+++ generics/index.go
@@ -4,8 +4,12 @@ package main
 import "fmt"
 
 
+// this `Index` function works for any type that supports comparison
+
 // Index returns the index of x in s, or -1 if not found.
 func Index[T comparable](s []T, x T) int {
+    // this declaration means that `s` is a slice of any type `T`
+    // that fulfills the built-in constraint `comparable`
     for i, v := range s {
         // v and x are type T, which has the comparable
         // constraint, so we can use `==` here
